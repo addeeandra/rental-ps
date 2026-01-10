103655a9eee0d290f95b20f7b8a83432c8ddc614
Aditya Chandra <aditchndr44@gmail.com>
2026-01-10 16:48:51 +0700

    chore: HeadingFormSection border to support dark mode

--- core/resources/js/components/HeadingFormSection.vue
+++ core/resources/js/components/HeadingFormSection.vue
@@ -8,7 +8,7 @@ defineProps<Props>();
 </script>
 
 <template>
-    <header class="border-b border-gray-200">
+    <header class="dark: border-b border-gray-200 dark:border-gray-800/50">
         <h3 class="mb-0.5 text-sm font-medium tracking-wide">
             {{ title }}
         </h3>
