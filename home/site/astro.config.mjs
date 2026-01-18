c0b44df0d54f0f5836c785f54c9d18b9b6a78afe
Aditya Chandra <aditchndr44@gmail.com>
2026-01-19 00:31:25 +0700

    adjust homesite

--- home/site/astro.config.mjs
+++ home/site/astro.config.mjs
@@ -19,6 +19,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://yourdomain.com',
+  site: 'https://xgameps.com',
   integrations: [ sitemap(), mdx()]
 });
